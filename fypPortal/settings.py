1ffcc9c2764c20aac50a0dbaf757a98765a48571
Musaddiq Habib <msdqhabib@gmail.com>
2023-04-30 23:11:52 +0500

    staticfiles update

--- fypPortal/settings.py
+++ fypPortal/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 MEDIA_URL = '/images/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR / 'static',]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
